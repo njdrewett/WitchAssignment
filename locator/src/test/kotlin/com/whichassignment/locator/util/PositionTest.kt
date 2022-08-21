c4c075e88f3b8719de714202bc198aeb70e706d4
njdrewett <njdrewett@sky.com>
2022-08-21 17:58:23 +0100

    Implmentation and Additional Tests

--- locator/src/test/kotlin/com/whichassignment/locator/util/PositionTest.kt
+++ locator/src/test/kotlin/com/whichassignment/locator/util/PositionTest.kt
@@ -1,7 +1,15 @@
 package com.whichassignment.locator.util
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 class PositionTest {
 
+    @Test
+    fun testPositionConstructor() {
+        var position = Position(0,0)
+
+        assertEquals(0, position.xPosition)
+        assertEquals(0, position.yPosition)
+    }
 }
